41538782c875027aae0e8d3cbf518d822c0e06b6
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-03-16 18:50:46 +0900

    Feat: Initial implementation of Integration classes

---
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/database/integration/Integration.kt
@@ -0,0 +1,98 @@
+package com.github.shunsukesudo.minecraft2fa.shared.database.integration
+
+interface Integration {
+    /**
+     *
+     * @param discordID Discord User ID
+     * @return true if information exists, otherwise false
+     */
+    fun isIntegrationInformationExists(discordID: Int): Boolean
+
+    /**
+     *
+     * @param MinecraftUUID Minecraft User UUID
+     * @return true if information exists, otherwise false
+     */
+    fun isIntegrationInformationExists(MinecraftUUID: String): Boolean
+
+    /**
+     *
+     * Search by player ID and MinecraftUUID and checks 2FA authentication information exists.
+     *
+     * @param DiscordID Discord User ID
+     * @param MinecraftUUID Minecraft User UUID
+     * @return true if information exists, otherwise false
+     */
+    fun isIntegrationInformationExists(DiscordID: Int, MinecraftUUID: String): Boolean
+
+    /**
+     *
+     * Adds integration information to database.
+     *
+     * @param discordID Discord User ID
+     * @param MinecraftUUID Minecraft User UUID
+     */
+    fun addIntegrationInformation(discordID: Int, MinecraftUUID: String)
+
+    /**
+     *
+     * Removes integration information from database
+     *
+     * @param discordID Discord User ID
+     * @param MinecraftUUID Minecraft User UUID
+     */
+    fun removeIntegrationInformation(discordID: Int, MinecraftUUID: String)
+
+    /**
+     *
+     * Search by Discord ID and Update Minecraft UUID to new UUID.
+     *
+     * @param discordID Discord User ID
+     * @param NewMinecraftUUID New Minecraft User UUID to update
+     */
+    fun updateIntegrationInformation(discordID: Int, NewMinecraftUUID: String)
+
+    /**
+     *
+     * Search by Minecraft UUID and Update Discord ID to new ID.
+     *
+     * @param MinecraftUUID Minecraft User UUID
+     * @param NewDiscordID New Discord ID to update
+     */
+    fun updateIntegrationInformation(MinecraftUUID: String, NewDiscordID: Int)
+
+    /**
+     * Search by Minecraft UUID and retrieves unique ID from database.
+     *
+     * @param MinecraftUUID Minecraft User UUID
+     * @return Unique User ID if found, otherwise -1.
+     */
+    fun getPlayerID(MinecraftUUID: String): Int
+
+    /**
+     *
+     * Search by Discord ID and retrieves unique ID from database.
+     *
+     * @param discordID Discord User ID
+     * @return Unique User ID if found, otherwise -1.
+     */
+    fun getPlayerID(discordID: Int): Int
+
+    /**
+     *
+     * Search by Discord ID and retrieves integrated Minecraft UUID from database.
+     *
+     * @param DiscordID Discord User ID
+     * @return Minecraft UUID if found, otherwise null.
+     */
+    fun getMinecraftUUIDFromDiscordID(DiscordID: Int): String?
+
+    /**
+     *
+     * Search by Minecraft UUID and retrieves integrated Discord ID from database.
+     *
+     * @param MinecraftUUID Minecraft User UUID
+     * @return Discord ID if found, otherwise -1.
+     */
+    fun getDiscordIDFromMinecraftUUID(MinecraftUUID: String): Int
+}
---
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/database/integration/IntegrationSQLiteImpl.kt
@@ -0,0 +1,45 @@
+package com.github.shunsukesudo.minecraft2fa.shared.database.integration
+
+internal class IntegrationSQLiteImpl(
+) : Integration {
+
+    override fun isIntegrationInformationExists(discordID: Int): Boolean {
+        return false
+    }
+
+    override fun isIntegrationInformationExists(MinecraftUUID: String): Boolean {
+        return false
+    }
+
+    override fun isIntegrationInformationExists(DiscordID: Int, MinecraftUUID: String): Boolean {
+        return false
+    }
+
+    override fun addIntegrationInformation(discordID: Int, MinecraftUUID: String){
+    }
+
+    override fun removeIntegrationInformation(discordID: Int, MinecraftUUID: String){
+    }
+
+    override fun updateIntegrationInformation(discordID: Int, NewMinecraftUUID: String){
+    }
+
+    override fun updateIntegrationInformation(MinecraftUUID: String, NewDiscordID: Int){
+    }
+
+    override fun getPlayerID(MinecraftUUID: String): Int {
+        return -1
+    }
+
+    override fun getPlayerID(discordID: Int): Int {
+        return -1
+    }
+
+    override fun getMinecraftUUIDFromDiscordID(DiscordID: Int): String? {
+        return null
+    }
+
+    override fun getDiscordIDFromMinecraftUUID(MinecraftUUID: String): Int {
+        return -1
+    }
+}
