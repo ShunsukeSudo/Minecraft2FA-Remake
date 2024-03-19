81c4eb4f1cd3ce411124f0cdbcd389e2d1884238
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-03-19 12:14:06 +0900

    Feat: add MCUserAuthentication class

---
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/authentication/MCUserAuthentication.kt
@@ -0,0 +1,36 @@
+package com.github.shunsukesudo.minecraft2fa.shared.authentication
+
+import java.util.UUID
+
+object MCUserAuthentication {
+
+    private val authorizedUser = HashMap<UUID, Boolean>()
+
+    /**
+     *
+     * Sets the user authorization status.
+     *
+     * @param minecraftUUID UUID of Player
+     * @param isAuthorized authorization status
+     */
+    @JvmStatic
+    fun setUserAuthorizedStatus(minecraftUUID: UUID ,isAuthorized: Boolean) {
+        authorizedUser[minecraftUUID] = isAuthorized
+    }
+
+    /**
+     *
+     * Checks user is authorized.
+     *
+     * @param minecraftUUID UUID of Player
+     * @return true if authorized, otherwise false
+     */
+    @JvmStatic
+    fun isUserAuthorized(minecraftUUID: UUID): Boolean{
+        // Return value of `authorizedUser[key]` is `Boolean?` not `Boolean` and need a null check so use when.
+        return when(authorizedUser[minecraftUUID]) {
+            true -> true
+            else -> false
+        }
+    }
+}
