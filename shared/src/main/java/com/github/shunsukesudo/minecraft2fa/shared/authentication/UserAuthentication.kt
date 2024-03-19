0941d05c009ce557d8fc05ea2cc4749c6daa0bb6
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-03-19 11:51:19 +0900

    Change: Make Authentication class more convenient. UserAuthentication's constructor is now private. UserAuthentication instance can be obtained from discordID

--- shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/authentication/UserAuthentication.kt
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/authentication/UserAuthentication.kt
@@ -3,36 +3,57 @@ package com.github.shunsukesudo.minecraft2fa.shared.authentication
 import com.warrenstrange.googleauth.GoogleAuthenticator
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey
 
-class UserAuthentication(
+class UserAuthentication private constructor(
     private val discordID: Long,
 ) {
     companion object {
         private val auth = GoogleAuthenticator()
+        private val credentials = HashMap<Long, UserAuthentication?>()
 
         @JvmStatic
         fun authorize(secretKey: String, TOTPCode: Int): Boolean {
             return auth.authorize(secretKey, TOTPCode)
         }
+
+
+        /**
+         *
+         * Creates a new credentials.
+         *
+         * @param discordID Discord User ID
+         * @return UserAuthentication instance
+         */
+        @JvmStatic
+        fun createNewCredentials(discordID: Long): UserAuthentication {
+            val cred = UserAuthentication(discordID)
+            credentials[discordID] = cred
+            return cred
+        }
+
+
+        /**
+         *
+         * Retrieves user credentials.
+         *
+         * @param discordID Discord User ID
+         * @return UserAuthentication instance if found, otherwise null
+         */
+        @JvmStatic
+        fun getCredentials(discordID: Long): UserAuthentication? {
+            return credentials[discordID]
+        }
     }
 
     private val gAuth = GoogleAuthenticator()
-    private var credentials: GoogleAuthenticatorKey? = null
+    private var credentials = gAuth.createCredentials(discordID.toString())
 
-    fun generateCredentials() {
-        credentials = gAuth.createCredentials(discordID.toString())
-    }
 
     fun getBackupCodes(): List<Int> {
-        if(credentials != null) {
-            return credentials!!.scratchCodes
-        }
-        return emptyList()
+        return credentials.scratchCodes
+
     }
 
-    fun getSecretKey(): String? {
-        if(credentials != null) {
-            return credentials!!.key
-        }
-        return null
+    fun getSecretKey(): String {
+        return credentials.key
     }
 }
