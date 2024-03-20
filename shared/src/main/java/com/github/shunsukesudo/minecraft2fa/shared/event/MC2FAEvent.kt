5f843b62b5daf02f1832216f99c1be06104e56f6
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-03-20 14:00:52 +0900

    Fix: Removed debug message from MC2FAEvent class

--- shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/event/MC2FAEvent.kt
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/event/MC2FAEvent.kt
@@ -10,22 +10,13 @@ class MC2FAEvent {
 
         @JvmStatic
         fun <T: GenericEvent> callEvent(event: T) {
-            println("Finding events")
             listeners.forEach { listenerClass ->
                 listenerClass::class.memberFunctions.forEach classes@{ func ->
-                    println("Finding annotated method")
                     if(!func.annotations.contains(EventHandler())) {
                         return@classes
                     }
-                    println("Annotated method found!")
-                    println("Annotated method name ${func.name}")
-                    println("Annotated method parameters count ${func.parameters.count()}")
                     func.parameters.forEach { param ->
-                        println("Val name: ${param.name}")
-                        println("Type: ${param.type}")
-                        println("Check type is same: ${param.type.javaType} | ${event.javaClass.typeName}")
                         if(param.type.javaType == event.javaClass) {
-                            println("Event match!")
                             func.call(listenerClass, event)
                         }
                     }
