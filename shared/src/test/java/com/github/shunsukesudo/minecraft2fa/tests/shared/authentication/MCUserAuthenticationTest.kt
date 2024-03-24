a00378edbe7c267757f3dd59667bb7b13814b445
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-03-25 08:42:47 +0900

    Test refactor: Tests now use random boolean and random UUID.

--- shared/src/test/java/com/github/shunsukesudo/minecraft2fa/tests/shared/authentication/MCUserAuthenticationTest.kt
+++ shared/src/test/java/com/github/shunsukesudo/minecraft2fa/tests/shared/authentication/MCUserAuthenticationTest.kt
@@ -4,29 +4,58 @@ import com.github.shunsukesudo.minecraft2fa.shared.authentication.MCUserAuthenti
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.collections.HashMap
+import kotlin.random.Random
 
 class MCUserAuthenticationTest {
 
+    private val random = Random(UUID.randomUUID().toString().filter { it.isDigit() }.toLong())
+
     @Test
-    fun `Test when uninitialized value accessed`() {
-        println("=========== Test when uninitialized value accessed")
+    fun `Test - Should return false when uninitialized value accessed`() {
+        println("=========== Test - Should return false when uninitialized value accessed")
         val expectedValue = false
+
         println("Calling: MCUserAuthentication.isUserAuthorized()")
         val actualValue = MCUserAuthentication.isUserAuthorized(UUID.randomUUID())
+
         println("Expected value: $expectedValue | Actual value: $actualValue")
         Assertions.assertEquals(expectedValue, actualValue)
+
+        println("Passed.")
     }
 
     @Test
-    fun `Test when initialized value accessed`() {
+    fun `Test - Should return individual value when initialized value accessed`() {
         println("=========== Test when initialized value accessed")
-        val expectedValue = true
-        val uuid = UUID.randomUUID()
-        println("Generated UUID: $uuid")
-        println("Calling: MCUserAuthentication.setUserAuthorizedStatus()")
-        MCUserAuthentication.setUserAuthorizedStatus(uuid, true)
-        val actualValue = MCUserAuthentication.isUserAuthorized(uuid)
-        println("Expected value: $expectedValue | Actual value: $actualValue")
-        Assertions.assertEquals(expectedValue, actualValue)
+
+        val uuids: MutableList<UUID> = mutableListOf()
+        val expectedValues: HashMap<UUID, Boolean> = HashMap()
+
+        for(i in 1..5) {
+            uuids.add(UUID.randomUUID())
+        }
+
+        var booleanVal: Boolean
+
+        uuids.forEach {
+            booleanVal = random.nextBoolean()
+            println("Generated UUID: $it")
+            println("Calling: MCUserAuthentication.setUserAuthorizedStatus() for user: $it")
+            MCUserAuthentication.setUserAuthorizedStatus(it, booleanVal)
+            expectedValues[it] = booleanVal
+        }
+
+        var actualValue: Boolean
+        var expectedValue: Boolean
+
+        uuids.forEach {
+            actualValue = MCUserAuthentication.isUserAuthorized(it)
+            expectedValue = expectedValues[it]!!
+            println("Expected value: $expectedValue | Actual value: $actualValue")
+            Assertions.assertEquals(expectedValue, actualValue)
+        }
+
+        println("Passed.")
     }
 }
