b07a340c6d64c515832992766d5eb39ce701b7ed
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-03-30 17:10:57 +0900

    Fix: bot token will not allow empty

--- shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/configuration/DiscordBotConfiguration.kt
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/configuration/DiscordBotConfiguration.kt
@@ -3,4 +3,8 @@ package com.github.shunsukesudo.minecraft2fa.shared.configuration
 data class DiscordBotConfiguration(
     val botToken: String
 ) {
+    init {
+        if(botToken.isEmpty())
+            throw IllegalArgumentException("Bot token is should not empty!")
+    }
 }
