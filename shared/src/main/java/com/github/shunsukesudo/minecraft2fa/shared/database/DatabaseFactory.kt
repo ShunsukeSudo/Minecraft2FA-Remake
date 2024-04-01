9ce70fd914f230a9933e8bc04ca869f91c93cd4f
Shunsuke Sudo <shunsuke.s0617@gmail.com>
2024-04-01 14:43:56 +0900

    Change: DatabaseFactory now only provide one newConnection method

--- shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/database/DatabaseFactory.kt
+++ shared/src/main/java/com/github/shunsukesudo/minecraft2fa/shared/database/DatabaseFactory.kt
@@ -1,17 +1,13 @@
 package com.github.shunsukesudo.minecraft2fa.shared.database
 
 import com.github.shunsukesudo.minecraft2fa.shared.configuration.DatabaseConfiguration
+import com.github.shunsukesudo.minecraft2fa.shared.configuration.DatabaseType
 
 object DatabaseFactory {
-    object SQLite {
-        fun newConnection(databaseConfiguration: DatabaseConfiguration): MC2FADatabase {
-            return SQLiteDatabase(databaseConfiguration)
-        }
-    }
-
-    object MySQL {
-        fun newConnection(databaseConfiguration: DatabaseConfiguration): MC2FADatabase {
-            return MySQLDatabase(databaseConfiguration)
+    fun newConnection(databaseConfiguration: DatabaseConfiguration): MC2FADatabase {
+        return when(databaseConfiguration.databaseType) {
+            DatabaseType.SQLITE -> SQLiteDatabase(databaseConfiguration)
+            DatabaseType.MYSQL -> MySQLDatabase(databaseConfiguration)
         }
     }
 }
